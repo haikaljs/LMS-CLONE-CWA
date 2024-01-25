707af21a802e9984fcfb57c21bea27c6bfe7812b
haikaljs <haikaljavascript@gmail.com>
2024-01-25 16:36:33 +0800

    Clerk authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700">This is protected page</p>
-   
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
