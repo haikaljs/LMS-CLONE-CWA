c6e1b04abbd71eaaef26b9637695e9976e26f5e5
haikaljs <haikaljavascript@gmail.com>
2024-01-25 16:05:34 +0800

    Folders setup

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700">Hello world</p>
-      <Button variant="outline">Button</Button>
+      <p className="text-3xl font-medium text-sky-700">This is protected page</p>
+   
     </div>
   );
 }
